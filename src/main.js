3a9523a07b322c31ab160175e5f0a40f7f8e4036
Greg <grtop@yandex.ru>
2023-07-07 13:09:57 +0300

    present

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
 import AppPresenter from './presenter/app-presenter';
+import FilterModel from './model/filter-model.js';
 
 
 const tripMainElement = document.querySelector('.trip-main');
 const filtersElement = tripMainElement.querySelector('.trip-controls__filters');
 const siteMainElement = document.querySelector('.trip-events');
+const filterModel = new FilterModel();
 
 const appComponent = new AppPresenter({
   tripMainElement,
   filtersElement,
-  siteMainElement
+  siteMainElement,
+  //destinationModel,
+  filterModel
 });
 
 appComponent.init();
--- src/view/trip-info-view.js
+++ src/view/trip-info-view.js
@@ -19,3 +19,39 @@ export default class TripInfoView extends AbstractView {
     return createTripInfoTemlpate();
   }
 }
+
+/*function createTripInfoTemplate(totalValue, startDate, endDate, tripWay) {
+  const tripWayDescription = tripWay.length > 3 ? tripWay.at(0).concat('&nbsp;&mdash;&nbsp;&hellip;&nbsp;&mdash;&nbsp', tripWay.at(-1)) : tripWay.join(' &mdash; ');
+  return (
+    `<section class="trip-main__trip-info  trip-info">
+      <div class="trip-info__main">
+        <h1 class="trip-info__title">${tripWayDescription}</h1>
+
+        <p class="trip-info__dates">${startDate}&nbsp;&mdash;&nbsp;${endDate}</p>
+      </div>
+
+      <p class="trip-info__cost">
+        Total: &euro;&nbsp;<span class="trip-info__cost-value">${totalValue}</span>
+      </p>
+    </section>`
+  );
+}
+
+export default class TripInfoView extends AbstractView {
+  #tripTotalValue = null;
+  #tripStartDate = null;
+  #tripEndDate = null;
+  #tripWay = [];
+
+  constructor ({tripTotalValue, tripStartDate, tripEndDate, tripWay}) {
+    super();
+    this.#tripTotalValue = tripTotalValue;
+    this.#tripStartDate = tripStartDate;
+    this.#tripEndDate = tripEndDate;
+    this.#tripWay = tripWay;
+  }
+
+  get template() {
+    return createTripInfoTemplate(this.#tripTotalValue, this.#tripStartDate, this.#tripEndDate, this.#tripWay);
+  }
+}*/
