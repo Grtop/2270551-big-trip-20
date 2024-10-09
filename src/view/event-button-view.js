cba72e0f7cabeb0dc635ad998e166602e8780cd5
Greg <grtop@yandex.ru>
2024-10-09 22:56:10 +0300

    ошибка при нажатии newPoint

--- src/view/event-button-view.js
+++ src/view/event-button-view.js
@@ -1,17 +1,4 @@
 // создание ДОМ кнопки New_Event
-/*import AbstractView from '../framework/view/abstract-view.js';
-
-const createButtonView = () =>
-  ` <div class="trip-main">
-     <button class="trip-main__event-add-btn  btn  btn--big  btn--yellow" type="button">New event</button>
-    </div>
-  `;
-
-export default class ButtonView extends AbstractView {
-  get template() {
-    return createButtonView();
-  }
-} */
 
 import AbstractView from '../framework/view/abstract-view.js';
 
